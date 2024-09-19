603d393887cc8b1343587ec65a5428cbdd1bbf64
leo-goldman <goldmanleo@grinnell.edu>
2024-09-18 23:39:28 -0500

    developed the bfregisterset class

--- src/main/java/edu/grinnell/csc207/utils/BFRegisterSet.java
+++ src/main/java/edu/grinnell/csc207/utils/BFRegisterSet.java
@@ -1,5 +1,39 @@
 package edu.grinnell.csc207.utils;
 
 public class BFRegisterSet {
-    
+
+  // +--------+-------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  BigFraction[] registry = new BigFraction[26];
+
+  // +--------------+-------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  public BFRegisterSet() {
+
+  }
+
+  // +---------+------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  //stores the given value in the specified register.
+  public void store(char register, BigFraction val) {
+    registry[General.l2i(register)] = val;
+  }
+
+  //retrieves the value from the given register.
+  public BigFraction get(char register) throws Exception {
+    BigFraction v = registry[General.l2i(register)];
+
+    if (v == null) {
+      throw new Exception("Invalid register " + register);
+    }
+    else {
+      return v;
+    }
+  }
 }
