86f8521672bf012f545de269a9304d439347a477
leo-goldman <goldmanleo@grinnell.edu>
2024-09-18 22:42:11 -0500

    developed the calculator.

--- src/main/java/edu/grinnell/csc207/utils/BFCalculator.java
+++ src/main/java/edu/grinnell/csc207/utils/BFCalculator.java
@@ -1,5 +1,54 @@
 package edu.grinnell.csc207.utils;
 
 public class BFCalculator {
+
+  // +--------+-------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  BigFraction lastVal;
+
+  // +--------------+-------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  public BFCalculator()
+  {
+    this.lastVal = new BigFraction(0, 1);
+  }
+
+  // +---------+------------------------------------------------------
+  // | Methods |
+  // +---------+
+
+  //gets the last computed value (returns 0 if there is no such value).
+  public BigFraction get() {
+    return this.lastVal;
+  }
+
+  //adds val to the last computed value.
+  public void add(BigFraction val) {
+    lastVal = lastVal.add(val);
+  }
+  //subtracts val from the last computed value.
+  public void subtract(BigFraction val) {
+    lastVal = lastVal.subtract(val);
+  }
+
+  //multiplies the last computed value by val.
+  public void multiply(BigFraction val) {
+    lastVal = lastVal.multiply(val);
+  }
+
+  //divides the last computed value by val.
+  public void divide(BigFraction val) {
+    lastVal = lastVal.divide(val);
+  }
+
+  //resets the last computed value to 0.
+  public void clear() {
+    this.lastVal = new BigFraction(0, 1);
+  }
+
     
 }
