a530cf3086b66067d63bbe8e53b8c21a1c9d7168
leo-goldman <goldmanleo@grinnell.edu>
2024-09-19 02:41:27 -0500

    small comments

--- src/main/java/edu/grinnell/csc207/utils/General.java
+++ src/main/java/edu/grinnell/csc207/utils/General.java
@@ -1,6 +1,12 @@
 package edu.grinnell.csc207.utils;
 
+import java.io.PrintWriter;
+
 public class General {
+
+  // +-------+--------------------------------------------------------
+  // | TOOLS |
+  // +-------+
   public static int l2i(char letter) {
     return (int) letter - (int) 'a';
   }
@@ -8,4 +14,12 @@ public class General {
   public static char i2l(int i) {
     return (char) (i + (int) 'a');
   }
+
+  // +------------+---------------------------------------------------
+  // | CALCULATOR |
+  // +------------+
+
+  // public static interpret_string(BFCalculator calc, BFRegisterSet regi) {}
+
+  //public static store_register(BFRegisterSet registry, )
 }
