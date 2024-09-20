1d8e78e3be5c6a9d1ac9a2088feab48191570e2d
leo-goldman <goldmanleo@grinnell.edu>
2024-09-19 20:57:59 -0500

    reformatted to work with general.java

--- src/main/java/edu/grinnell/csc207/main/InteractiveCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/InteractiveCalculator.java
@@ -1,23 +1,13 @@
 package edu.grinnell.csc207.main;
 
 import java.io.PrintWriter;
-import java.math.BigInteger;
 import java.util.Scanner;
 
 import edu.grinnell.csc207.utils.BFCalculator;
 import edu.grinnell.csc207.utils.BFRegisterSet;
-import edu.grinnell.csc207.utils.BigFraction;
+import edu.grinnell.csc207.utils.General;
 
 public class InteractiveCalculator {
-
-  private static final int NONE = -1;
-  private static final int NUM = 0;
-  private static final int ADD_NUM = 1;
-  private static final int SUB_NUM = 2;
-  private static final int MUL_NUM = 3;
-  private static final int DIV_NUM = 4;
-  private static final int STORE_NUM = 5;
-  private static final int ERR = 6;
 
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
@@ -33,107 +23,12 @@ public class InteractiveCalculator {
       pen.flush();
       String str = eyes.nextLine();
 
-      BFCalculator cal = new BFCalculator();
-
-      int prev_cmd = NONE;
-
       if (str.equals("QUIT")) {
         eyes.close();
         return;
       }
-      String[] cmds = str.split(" ");
-      
-      parse: for (String s : cmds) {
-        if (s.length() == 0) {
-          continue;
-        }
-        if (prev_cmd == NUM) {
-          if (s.equals("+")) {
-            prev_cmd = ADD_NUM;
-          }
-          else if (s.equals("-")) {
-            prev_cmd = SUB_NUM;
-          }
-          else if (s.equals("*")) {
-            prev_cmd = MUL_NUM;
-          }
-          else if (s.equals("/")) {
-            prev_cmd = DIV_NUM;
-          }
-          else {
-            System.err.println("ERROR: Lacking a mathematical constructor");
-            prev_cmd = ERR;
-            break parse;
-          }
-        }
-        else if (prev_cmd == STORE_NUM) {
-          if (BFRegisterSet.valid_register(s)) {
-            regi.store(s.charAt(0), calc.get());
-            pen.println("STORED " + s + " -> " + calc.get().toString());
-          }
-          else {
-            System.err.println("ERROR: STORE command received invalid register: " + s + " (register ignored)");
-          }
-        }
-        else {
-          // Check if s is a STORE command.
-          if (s.equals("STORE") && prev_cmd == NONE) {
-            prev_cmd = STORE_NUM;
-          }
-          else {
-            BigFraction f;
-            // Check if s is a valid register.
-            if (BFRegisterSet.valid_register(s)) {
-              f = regi.get(s.charAt(0));
-            }
-            else {
-              f = new BigFraction(s);
-            }
-            
-            if (f.denominator() == BigInteger.ZERO) {
-              System.err.println("ERROR: Invalid expression");
-              prev_cmd = ERR;
-              break parse;
-            }
 
-            f = f.simplify();
-            switch (prev_cmd) {
-              case NONE:
-                cal.set(f);
-                prev_cmd = NUM;
-                break;
-              case ADD_NUM:
-                cal.add(f);
-                prev_cmd = NUM;
-                break;
-              case SUB_NUM:
-                cal.subtract(f);
-                prev_cmd = NUM;
-                break;
-              case MUL_NUM:
-                cal.multiply(f);
-                prev_cmd = NUM;
-                break;
-              case DIV_NUM:
-                cal.divide(f);
-                prev_cmd = NUM;
-                break;
-              default:
-                System.err.println("ERROR: This is a very bad sign");
-                prev_cmd = ERR;
-                break parse;
-            }
-            //pen.printf(f.numerator().toString() + "/" + f.denominator().toString() + " ");
-          }
-        }
-      } // string parse
-      if (prev_cmd == NUM) {
-        pen.println(cal.get().toString());
-        calc.set(cal.get());
-      }
-      else if (prev_cmd < 5 && prev_cmd > 0) {
-        System.err.println("ERROR: Invalid expression");
-      }
+      General.process_str(str, calc, regi, pen);
     } // command loop
   } // main
 }
