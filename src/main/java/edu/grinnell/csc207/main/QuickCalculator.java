0fdb550545269dc5ebc35ceaed5df6faac2a172a
leo-goldman <goldmanleo@grinnell.edu>
2024-09-19 20:58:13 -0500

    added to work with general.java

--- src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
@@ -1,13 +1,22 @@
 package edu.grinnell.csc207.main;
 
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 import edu.grinnell.csc207.utils.BFCalculator;
 import edu.grinnell.csc207.utils.BFRegisterSet;
+import edu.grinnell.csc207.utils.General;
 
 public class QuickCalculator {
     public static void main(String[] args) {
-      
+      PrintWriter pen = new PrintWriter(System.out, true);
+
+      //create a calculator
+      BFCalculator calc = new BFCalculator();
+      //create a registry
+      BFRegisterSet regi = new BFRegisterSet();
+
+      for (String str : args) {
+        General.process_str(str, calc, regi, pen);
+      }
     }
 }
