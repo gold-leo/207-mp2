6e4577b7d61c442f07771f80ae46dc25b9212c20
leo-goldman <goldmanleo@grinnell.edu>
2024-09-19 20:45:21 -0500

    added main function

--- src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
+++ src/main/java/edu/grinnell/csc207/main/QuickCalculator.java
@@ -7,5 +7,7 @@ import edu.grinnell.csc207.utils.BFCalculator;
 import edu.grinnell.csc207.utils.BFRegisterSet;
 
 public class QuickCalculator {
-    
+    public static void main(String[] args) {
+      
+    }
 }
