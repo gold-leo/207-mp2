3eeb9d4631f1fd7c198955056710f31f115d18fe
gold-leo <goldmanleo@grinnell.edu>
2024-11-11 22:29:12 -0600

    comments

--- src/main/java/edu/grinnell/csc207/utils/BFRegisterSet.java
+++ src/main/java/edu/grinnell/csc207/utils/BFRegisterSet.java
@@ -1,7 +1,7 @@
 package edu.grinnell.csc207.utils;
 
 /**
- * Registry for BigFractions.
+ * Registry for BigFractions. Stores up to 26 values (a-z). 
  */
 public class BFRegisterSet {
 
@@ -10,7 +10,7 @@ public class BFRegisterSet {
   // +--------+
 
   /**
-   * Registry.
+   * The array which stores the data.
    */
   BigFraction[] registry = new BigFraction[26];
 
@@ -20,12 +20,13 @@ public class BFRegisterSet {
 
   /**
    * Constructor for the registry.
+   * Sets all registries to 0/0 BigFractions.
    */
   public BFRegisterSet() {
     for (int i = 0; i < 26; i++) {
       registry[i] = new BigFraction();
     } // for
-  } // constructor
+  } // BFRegisterSet()
 
   // +---------+------------------------------------------------------
   // | Methods |
@@ -34,35 +35,41 @@ public class BFRegisterSet {
   /**
    * Checks if the str is a valid register character.
    * @param str
-   * @return t or f
+   * @return true or false
    */
   public static boolean validRegister(String str) {
-    if (str.length() > 1) {
+
+    // Safety check: str must have a length of 1.
+    if (str.length() > 1 || str.length() < 1) {
       return false;
     } // if
+
+    // Check if the character in str is a valid register char.
     int i = General.l2i(str.charAt(0));
     if (i < 0 || i > 25) {
       return false;
     } // if
     return true;
-  } // valid_register
+  } // validRegister(str)
 
   /**
-   * stores the val in the register.
+   * Stores the val in the register.
+   * Prereq: register must be a valid register character. 
    * @param val
    * @param register
    */
   public void store(char register, BigFraction val) {
     registry[General.l2i(register)] = val;
-  } //store
+  } // store(register, val)
 
   /**
-   * retrieves the value from register.
+   * Retrieves the value from register.
+   * Prereq: register must be a valid register character. 
    * @param register
-   * @return v
+   * @return BigFraction v
    **/
   public BigFraction get(char register) {
     BigFraction v = registry[General.l2i(register)];
     return v;
-  } // get
-} // class
+  } // get(register)
+} // BFRegisterSet
